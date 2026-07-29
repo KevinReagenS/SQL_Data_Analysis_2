38c19024aebc5eb0ded2e782bcdd93b3c7d42ab6
KevinReagenS <kevin.reagen03@gmail.com>
2026-07-29 12:58:00 +0700

    Question 3 - Halfway

--- project_sql/3_customers_status.sql
+++ project_sql/3_customers_status.sql
@@ -4,7 +4,8 @@ WITH last_purchase AS (
 		ca.full_name,
 		ca.first_purchase_date,
 		ca.orderdate,
-		ROW_NUMBER() OVER (PARTITION BY ca.customerkey ORDER BY ca.orderdate DESC) AS rn
+		ROW_NUMBER() OVER (PARTITION BY ca.customerkey ORDER BY ca.orderdate DESC) AS rn,
+		ca.cohort_year
 	FROM
 		cohort_analysis ca
 ), customer_status AS (
@@ -12,6 +13,7 @@ SELECT
 	customerkey,
 	full_name,
 	orderdate AS last_purchase_date,
+	cohort_year,
 	CASE
 		WHEN orderdate < (SELECT (MAX(orderdate)) FROM sales) - INTERVAL '6 months' THEN 'Churned'
 		ELSE 'Active'
@@ -24,11 +26,13 @@ WHERE
 )
 
 SELECT
+	cs.cohort_year,
 	cs.customer_status,
 	COUNT(cs.customerkey) AS customer_counts,
-	SUM(COUNT(cs.customerkey)) OVER () AS total_customers,
-	ROUND(100 * (COUNT(cs.customerkey) / SUM(COUNT(cs.customerkey)) OVER ()), 2) AS customer_status_pct
+	SUM(COUNT(cs.customerkey)) OVER (PARTITION BY cohort_year) AS total_customers,
+	ROUND(100 * (COUNT(cs.customerkey) / SUM(COUNT(cs.customerkey)) OVER (PARTITION BY cohort_year)), 2) AS customer_status_pct
 FROM
 	customer_status cs
 GROUP BY
+	cs.cohort_year,
 	cs.customer_status
